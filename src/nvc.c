719b0a18ac1cc2fe5127460e82b742290768d5be
Kevin Klues <kklues@nvidia.com>
2021-02-02 06:19:15 -0800

    Add more logging around device node creation with --load-kmods
    Signed-off-by: Kevin Klues <kklues@nvidia.com>

--- src/nvc.c
+++ src/nvc.c
@@ -193,9 +193,11 @@ load_kernel_modules(struct error *err, const char *root)
                 if (nvidia_modprobe(0) == 0)
                         log_err("could not load kernel module nvidia");
                 else {
+                        log_info("running mknod for " NV_CTL_DEVICE_PATH);
                         if (nvidia_mknod(NV_CTL_DEVICE_MINOR) == 0)
                                 log_err("could not create kernel module device node");
                         for (int i = 0; i < (int)devs.num_matches; ++i) {
+                                log_infof("running mknod for " NV_DEVICE_PATH, i);
                                 if (nvidia_mknod(i) == 0)
                                         log_err("could not create kernel module device node");
                         }
@@ -205,6 +207,7 @@ load_kernel_modules(struct error *err, const char *root)
                 if (nvidia_uvm_modprobe() == 0)
                         log_err("could not load kernel module nvidia_uvm");
                 else {
+                        log_info("running mknod for " NV_UVM_DEVICE_PATH);
                         if (nvidia_uvm_mknod(0) == 0)
                                 log_err("could not create kernel module device node");
                 }
@@ -213,6 +216,7 @@ load_kernel_modules(struct error *err, const char *root)
                 if (nvidia_modeset_modprobe() == 0)
                         log_err("could not load kernel module nvidia_modeset");
                 else {
+                        log_info("running mknod for " NV_MODESET_DEVICE_PATH);
                         if (nvidia_modeset_mknod() == 0)
                                 log_err("could not create kernel module device node");
                 }
