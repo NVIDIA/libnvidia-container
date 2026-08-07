1d5dcc7c448b9090a8132a4bdaf63f15235dcf4a
Rajat Chopra <rajatc@nvidia.com>
2026-08-07 13:57:06 -0700

    check length of 'root' string before writing to ctx->root
    Signed-off-by: Rajat Chopra <rajatc@nvidia.com>

--- src/driver.c
+++ src/driver.c
@@ -89,6 +89,10 @@ driver_init(struct error *err, struct dxcore_context *dxcore, const char *root, 
                 .gid = gid,
                 .nvml_dl = NULL,
         };
+        if (strlen(root) > sizeof(ctx->root)) {
+            error_setx(err, "root path too long (max %zu bytes)", sizeof(ctx->root) - 1);
+            goto fail;
+        }
         strcpy(ctx->root, root);
 
         if (dxcore->initialized) {
